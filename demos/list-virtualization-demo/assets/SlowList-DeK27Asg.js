53a790d639c5e0a5b4cafdc2cac9cb3b8e69e0a0
tahmidrahman <tahmid131@gmail.com>
2026-05-02 23:10:48 +0600

    add list-virtualization demo

---
+++ demos/list-virtualization-demo/assets/SlowList-DeK27Asg.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./jsx-runtime-D0TZn2ta.js";import{t as n}from"./ListItem-BkA6M3F7.js";var r=t();function i(){return(0,r.jsx)(`div`,{className:`slow-list`,children:e.map(e=>(0,r.jsx)(n,{item:e},e.id))})}export{i as SlowList};
